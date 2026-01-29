f3b1e24b6883e22b3168ea88502736eb7928f9a5
xVrukx <developeryuvrajsirganor@gmail.com>
2026-01-29 19:12:17 +0530

    signup auth.routes  client.model bank.model auth.controller

--- finn4sure-frontend/src/pages/Signup.jsx
+++ finn4sure-frontend/src/pages/Signup.jsx
@@ -8,33 +8,74 @@ export default function Signup() {
 
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
+  const [number, setnumber] = useState("");
+  const [recivedotp, setrecivedotp] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!fullName.trim() || !email.trim() || !mobile.trim() || !password.trim()) {
-      setError("All fields are required.");
-      return;
+  // otpsender
+  const sendOTP = async(number) => {
+    const res = await fetch("http://localhost:5000/auth/sendOTP",{
+      method : "POST",
+      headers : {"content-Type" : "application/json"},
+      body : JSON.stringify({number})
+    })
+    if(!res.ok){
+      return res.json({message : "error occore while sending otp"})
     }
-
-    if (mobile.length !== 10) {
-      setError("Please enter a valid 10-digit mobile number.");
-      return;
-    }
-
-    setError("");
-
-    // Mock signup → auto login
-    login({
-      name: fullName,
-      email,
-    });
-
-    navigate("/apply");
+    return res.status(200).json({message : "OTP was sent sucessfully"})
   }
+
+  const VerifyOTP = async() => {
+    const res = await fetch("http://localhost:5000/auth/VerifyOTP",{
+      method : "POST",
+      headers : {"content/Type" : "application/json"},
+      body : JSON.stringify({recivedotp, number})
+    })
+    if(!res.ok){
+      return await res.status(402).json({message : "error occored while verifying user"})
+    }
+    return res.status(200).json({message : "user verified"})
+  }
+
+  const FormSender = async() => {
+    const res = await fetch("http://localhost:5000/auth/signup",{
+      method : "POST",
+      headers : {"content-Type" : "application/json"},
+      body : JSON.stringify({
+        name : fullName, email : email,
+        number : number, password : password,
+        role : "client"
+      })
+    })
+    if(!res.ok){
+      return res.status(402).json({message : "error occored while sending the data back to the server"})
+    }
+    return res.status(200).json({message : "user singuped successfully"})
+  }
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+
+  //   if (!fullName.trim() || !email.trim() || !number.trim() || !password.trim()) {
+  //     setError("All fields are required.");
+  //     return;
+  //   }
+
+  //   if (number.length !== 10) {
+  //     setError("Please enter a valid 10-digit number number.");
+  //     return;
+  //   }
+
+  //   setError("");
+
+  //   // Mock signup → auto login
+  //   login({
+  //     name: fullName,
+  //     email,
+  //   });
+
+  //   navigate("/apply");
+  // }
 
   return (
     <section className="bg-linear-to-b from-blue-50 via-white to-white min-h-screen flex items-center">
@@ -48,7 +89,7 @@ export default function Signup() {
           Sign up to start your loan application journey.
         </p>
 
-        <form onSubmit={handleSubmit} className="mt-6 space-y-5">
+        <div className="mt-6 space-y-5">
           {error && (
             <div className="p-3 bg-red-50 text-red-700 rounded-lg text-sm">
               {error}
@@ -85,23 +126,6 @@ export default function Signup() {
 
           <div>
             <label className="block text-sm font-medium text-slate-700">
-              Mobile Number
-            </label>
-            <input
-              type="tel"
-              placeholder="10-digit mobile number"
-              value={mobile}
-              onChange={(e) => {
-                const value = e.target.value.replace(/\D/g, "");
-                if (value.length <= 10) setMobile(value);
-              }}
-              className="mt-2 w-full px-4 py-3 border border-slate-300 rounded-lg
-                         focus:outline-none focus:ring-2 focus:ring-blue-600"
-            />
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-slate-700">
               Password
             </label>
             <input
@@ -114,16 +138,54 @@ export default function Signup() {
             />
           </div>
 
+          <div>
+            <label className="block text-sm font-medium text-slate-700">
+              number Number
+            </label>
+            <input
+              type="tel"
+              placeholder="10-digit number number"
+              value={number}
+              onChange={(e) => {
+                const value = e.target.value.replace(/\D/g, "");
+                if (value.length <= 10) setnumber(value);
+              }}
+              className="mt-2 w-full px-4 py-3 border border-slate-300 rounded-lg
+                         focus:outline-none focus:ring-2 focus:ring-blue-600"
+            />
+            <button className="bg-black hover:bg-black/80 transition duration-500
+             h-12 w-20 rounded-md m-2 text-center text-white"
+            onClick={sendOTP()}
+            >Send OTP</button>
+          </div>
+          
+          <div>
+            <input type="text"placeholder="Verify OTP" className="mt-2 w-full
+            px-4 py-3 border border-slate-300 rounded-lg focus:outline-none
+            focus:ring-2 focus:ring-blue-600"
+            value={recivedotp}
+            onChange={(e) => {setrecivedotp(e.target.value)}} />
+
           <button
-            type="submit"
+            className="bg-black hover:bg-black/80 transition duration-500
+             h-12 w-20 rounded-md m-2 text-center text-white"
+            type="button" onClick={VerifyOTP()}
+          >
+            Verify OTP
+          </button>
+
+          </div>
+
+          <button
             className="w-full py-3 rounded-lg font-medium text-white
                        bg-linear-to-r from-blue-700 via-teal-600 to-emerald-500
                        hover:from-blue-800 hover:via-teal-700 hover:to-emerald-600
                        transition"
+            type="button" onClick={FormSender()}
           >
-            Create Account
+            Submit
           </button>
-        </form>
+        </div>
 
         <p className="mt-4 text-sm text-slate-600 text-center">
           Already have an account?{" "}
