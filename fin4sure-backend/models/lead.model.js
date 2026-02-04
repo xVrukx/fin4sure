89fe83c02c196142070c41b1a26790260fa66f0d
xVrukx <developeryuvrajsirganor@gmail.com>
2026-02-04 15:51:41 +0530

    auth update 2-2-26

--- fin4sure-backend/models/lead.model.js
+++ fin4sure-backend/models/lead.model.js
@@ -7,7 +7,7 @@ const leadSchema = new mongoose.Schema(
         number : {type : String, required : true},
         pan_card : {type : String, sparse : true, unique : true},
         product : [{type : String, sparse : true}],
-        status : 
+        status : {type : String, required : true, enum : ["approved", "rejected", "pending"], default : "pending"},
         broker_id : {type : String, default : "self"}
     },
     {
