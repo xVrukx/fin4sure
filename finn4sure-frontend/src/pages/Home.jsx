a46a880e86b3762ddfbaac0015c018dc941e6d1c
xVrukx <developeryuvrajsirganor@gmail.com>
2026-04-25 18:40:39 +0530

    1.1.5v

--- finn4sure-frontend/src/pages/Home.jsx
+++ finn4sure-frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/ui/Hero";
 import LoanProductsGrid from "../components/ui/LoanProductsGrid";
+import BankCarousel from "../components/ui/carousal";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <LoanProductsGrid />
+      <BankCarousel />
     </>
   );
 }
