ee881f6d9653ba3bdb8062b8b17609e5aaf86fa5
xVrukx <developeryuvrajsirganor@gmail.com>
2026-04-25 15:20:55 +0530

    1.0.3

--- finn4sure-frontend/src/pages/Brokerdashboard.jsx
+++ finn4sure-frontend/src/pages/Brokerdashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import validator from "validator";
 import {
   IoMdNotifications,
   IoMdTimer,
