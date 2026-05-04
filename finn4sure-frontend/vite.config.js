bb9b23e71c9e37f8a601c6abbd17463845711bb5
xVrukx <developeryuvrajsirganor@gmail.com>
2026-05-04 10:16:59 +0530

    1.4.4v{beta}

--- finn4sure-frontend/vite.config.js
+++ finn4sure-frontend/vite.config.js
@@ -4,5 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),],
+  resolve:{
+    alias:{
+      "@": path.resolve(__dirname,"src")
+    }
+  },
 });
