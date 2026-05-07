8874e8904896586ff894ce2b0cbbc34aff97e281
xVrukx <developeryuvrajsirganor@gmail.com>
2026-05-07 12:09:26 +0530

    1.4.25{beta}v

--- finn4sure-frontend/src/pages/Calculator.jsx
+++ finn4sure-frontend/src/pages/Calculator.jsx
@@ -176,7 +176,7 @@ export default function Calculator() {
               </div>
             </div>
 
-            <button onClick={!isAuthenticated?navigate("/login"):navigate("/apply")}
+            <button onClick={() => {!isAuthenticated?navigate("/login"):navigate("/apply")}}
               className="mt-5 block text-center px-6 py-4 rounded-xl font-medium text-white
                          bg-linear-to-r from-blue-700 via-teal-600 to-emerald-500
                          hover:scale-[1.02] transition"
